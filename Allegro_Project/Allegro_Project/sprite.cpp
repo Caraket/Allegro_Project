6eb8f3d70511adb8d523f72d96db4e329b077c06
Cory <cory.paulley@yahoo.com>
2017-11-10 11:41:37 -0600

    sprite
    Added very basic funtionality to the sprite class.

--- Allegro_Project/Allegro_Project/sprite.cpp
+++ Allegro_Project/Allegro_Project/sprite.cpp
@@ -10,8 +10,25 @@ void sprite::Destroy() {
 	Object::Destroy();
 }
 
-void sprite::Init(ALLEGRO_BITMAP *image, ALLEGRO_DISPLAY *display) {
+void sprite::Init(ALLEGRO_BITMAP *image, ALLEGRO_DISPLAY *display, int m_maxFrame, int m_animationColumns, int m_animationRows) {
 	Object::Init(100, 200, 0, 0);
+	displayWidth = al_get_display_width(display);
+	displayHeight = al_get_display_height(display);
+
+	bitmapWidth = al_get_bitmap_width(image);
+	bitmapHeight = al_get_bitmap_height(image);
+
+	frameWidth = bitmapWidth / animationColumns;
+	frameHeight = bitmapHeight / animationRows;
+
+
+
+	if (image != NULL)
+		sprite::image = image;
+
+	if (display != NULL)
+		sprite::display = display;
+
 }
 
 void sprite::Update() {
--- Allegro_Project/Allegro_Project/sprite.h
+++ Allegro_Project/Allegro_Project/sprite.h
@@ -14,12 +14,14 @@ protected:
 	int currentFrame;
 	int frameCount;
 	int frameDelay;
+	int frameWidth;
+	int frameHeight;
 	int animationDirection;
 
 public:
 	sprite();
 	void virtual Destroy();
-	void virtual Init(ALLEGRO_BITMAP *image = NULL, ALLEGRO_DISPLAY *display = NULL);
+	void virtual Init(ALLEGRO_BITMAP *image = NULL, ALLEGRO_DISPLAY *display = NULL, int m_maxFrame, int m_animationColumns, int m_animationRows);
 
 	void virtual Update();
 	void Render();
